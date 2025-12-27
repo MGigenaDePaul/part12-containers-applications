551e2bbf5219d6c29bd8381603cac8d71ca915f6
Miqueas <proyectosmiqueas@gmail.com>
2025-12-27 07:06:00 +0000

    Exercise 12.17

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    allowedHosts: [
+      'todo-frontend-dev',
+      'localhost',
+      '.localhost'
+    ],
+    watch: {
+      usePolling: true
+    }
+  },
   test: {
     environment: 'jsdom',
     globals: true,
