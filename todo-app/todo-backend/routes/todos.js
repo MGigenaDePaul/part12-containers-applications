336339d3fe3f8257fc28dcc30b165557b8ab62df
Miqueas <proyectosmiqueas@gmail.com>
2025-12-27 02:13:41 +0000

    Exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,22 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.json(req.todo); // Implement this
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body; // Implement this
+
+  if (text !== undefined) {
+    req.todo.text = text;
+  }
+  if (done !== undefined) {
+    req.todo.done = done;
+  }
+
+  await req.todo.save();
+  res.json(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
